ee0a24bbe5287b73409a68717e5e15350396904e
fsfarmscaper <fs.farmscaper@gmail.com>
2026-04-16 21:57:08 +1200

    Enable USB CDC for debugging

--- firmware/src/tusb_config_host/tusb_config.h
+++ firmware/src/tusb_config_host/tusb_config.h
@@ -1,7 +1,7 @@
 #ifndef _TUSB_CONFIG_H_
 #define _TUSB_CONFIG_H_
 
-#define CFG_TUSB_RHPORT0_MODE (OPT_MODE_HOST | OPT_MODE_FULL_SPEED)
+#define CFG_TUSB_RHPORT0_MODE (OPT_MODE_HOST | OPT_MODE_DEVICE | OPT_MODE_FULL_SPEED)
 
 #define CFG_TUSB_MEM_SECTION
 #define CFG_TUSB_MEM_ALIGN __attribute__((aligned(4)))
@@ -18,5 +18,13 @@
 
 #define CFG_TUH_HID_EPIN_BUFSIZE 64
 #define CFG_TUH_HID_EPOUT_BUFSIZE 64
+
+// Device-side configuration for CDC debug output
+#define CFG_TUD_ENDPOINT0_SIZE 64
+#define CFG_TUD_CDC 1
+#define CFG_TUD_HID 0
+#define CFG_TUD_MSC 0
+#define CFG_TUD_MIDI 0
+#define CFG_TUD_VENDOR 0
 
 #endif
