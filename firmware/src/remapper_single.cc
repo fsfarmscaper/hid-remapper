08d55c88906a201e8fefb9a2277d7341562fe790
fsfarmscaper <fs.farmscaper@gmail.com>
2026-04-26 21:09:32 +1200

    Adjust init for G923

--- firmware/src/remapper_single.cc
+++ firmware/src/remapper_single.cc
@@ -110,7 +110,8 @@ void read_report(bool* new_report, bool* tick) {
         printf("G923: hub port reset (hub=%d port=%d)\n",
                g923_reset_hub_addr, g923_reset_hub_port);
 #endif
-        tuh_hub_port_reset(g923_reset_hub_addr, g923_reset_hub_port, NULL);
+        // TODO: PA error: 'tuh_hub_port_reset' was not declared in this scope
+        //tuh_hub_port_reset(g923_reset_hub_addr, g923_reset_hub_port, NULL);
         g923_reset_hub_addr = 0;
         g923_reset_hub_port = 0;
     }    
